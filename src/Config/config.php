b42b5d08f66b0fd20f256c54feb812807aa80908
bigrocs <bigrocs@qq.com>
2017-10-07 14:17:08 +0800

    增加广播认证服务

--- src/Config/config.php
+++ src/Config/config.php
@@ -10,6 +10,7 @@ return [
     'install' => 'corecmf:broadcasting:install',//安装artisan命令
     'uninstall' => 'corecmf:broadcasting:uninstall',//卸载artisan命令
     'providers' => [
+      App\Providers\BroadcastServiceProvider::class,//广播认证服务
       CoreCMF\Broadcasting\Providers\EventServiceProvider::class,//事件服务
     ],
 ];

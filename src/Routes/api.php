79ec15eaa76871eab8be576ef4bde99bc1f7297a
bigrocs <bigrocs@qq.com>
2017-10-07 10:56:53 +0800

    config路由

--- src/Routes/api.php
+++ src/Routes/api.php
@@ -10,7 +10,7 @@
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::group(['prefix' => 'api', 'middleware' => 'api', 'namespace' => 'CoreCMF\Omnipay\App\Http\Controllers\Api', 'as' => 'api.'], function () {
+Route::group(['prefix' => 'api', 'middleware' => 'api', 'namespace' => 'CoreCMF\Broadcasting\App\Http\Controllers\Api', 'as' => 'api.'], function () {
     /*
     |--------------------------------------------------------------------------
     | 需要用户认证路由模块
@@ -18,13 +18,10 @@ Route::group(['prefix' => 'api', 'middleware' => 'api', 'namespace' => 'CoreCMF\
     */
     Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => []], function () {
     	// 后台仪表盘路由
-	    Route::group(['prefix' => 'omnipay', 'as' => 'omnipay.'], function () {
+	    Route::group(['prefix' => 'broadcasting', 'as' => 'broadcasting.'], function () {
         //配置
 		    Route::post('config',                ['as' => 'config',     'uses' => 'ConfigController@index']);
         Route::post('config/update',         ['as' => 'config.update','uses' => 'ConfigController@update']);
-        Route::post('config/file',           ['as' => 'config.file','uses' => 'ConfigController@file']);
-        //订单
-        Route::post('order',                 ['as' => 'order',     'uses' => 'OrderController@index']);
 		  });
 
 	 });
